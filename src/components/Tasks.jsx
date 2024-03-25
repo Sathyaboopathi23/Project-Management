7f409ad8876b2ea9df96b202bc55715fd5f3851b
Sathyaboopathi23 <149357957+Sathyaboopathi23@users.noreply.github.com>
2024-03-25 14:43:00 +0530

    Add files via upload

---
+++ src/components/Tasks.jsx
@@ -0,0 +1,30 @@
+import NewTask from "./NewTask.jsx";
+
+export default function Tasks({ tasks, onAdd, onDelete }) {
+  return (
+    <section>
+      <h1 className="text-2xl text-stone-700 font-bold mb-4">Tasks</h1>
+      <NewTask onAdd={onAdd} />
+      {tasks.length === 0 && (
+        <p className="text-stone-800 my-4">
+          This project does not have any tasks yet.
+        </p>
+      )}
+      {tasks.length > 0 && (
+        <ul className="p-4 mt-8 rounded-md bg-stone-100">
+          {tasks.map((task) => (
+            <li key={task.id} className="flex justify-between my-4">
+              <span>{task.text}</span>
+              <button
+                className="text-stone-700 hover:text-stone-900"
+                onClick={() => onDelete(task.id)}
+              >
+                Clear
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </section>
+  );
+}
